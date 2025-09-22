2b4746ca90cb6720617150a4f48b97c05074dbb2
Wisdom Ofogba <wisetega007@gmail.com>
2025-09-22 16:55:33 +0100

    finished auth for users

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import { login, register, resendOTP, verifyOTP, forgotPassword, resetPassword } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/auth/register", register);
 
 router.post("/auth/login", login);
+
+router.post("/auth/verify-otp", verifyOTP)
+
+router.post("/auth/resend-otp", resendOTP)
+
+router.post("/auth/forgot-password", forgotPassword)
+
+router.post("/auth/reset-password", resetPassword)
 
 export default router;
