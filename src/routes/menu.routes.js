e460416432afbbc86f96a1ddf4188b656ad342b9
Wisdom Ofogba <wisetega007@gmail.com>
2025-10-21 11:54:22 +0100

    fixed menu

--- src/routes/menu.routes.js
+++ src/routes/menu.routes.js
@@ -25,7 +25,7 @@ router.put("/:id", protect, uploadMenuImages, updateMenu);
 router.delete("/:id", protect, deleteMenu);
 
 router.post("/items", protect, createMenuItem);
-router.get("/items", protect, getMenuItems);
+router.get("/items", getMenuItems);
 router.get("/items/export-csv", protect, exportMenuItemsCSV); // New route for exporting menu items
 router.put("/items/:id", protect, updateMenuItem);
 router.delete("/items/:id", protect, deleteMenuItem);
