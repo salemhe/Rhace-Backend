7667acbe0b5f7bb0232f82e44a49c2f530e6088a
Samocology <godstimesml@gmail.com>
2025-11-29 00:35:04 +0100

    added admin signup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -6,11 +6,13 @@ import {
   resetPassword,
   verifyOTP, // New import
   resendOTP, // New import
+  registerAdmin,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/register-admin", registerAdmin);
 router.post("/login", login);
 router.post("/verify-otp", verifyOTP); // New route
 router.post("/resend-otp", resendOTP); // New route
