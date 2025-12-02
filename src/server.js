a39ba3ec08eb54669e37976b51d1606b102a3dc1
Samocology <godstimesml@gmail.com>
2025-12-02 10:24:52 +0100

    made socket.io CORS to be universal

--- src/server.js
+++ src/server.js
@@ -1,38 +1,51 @@
 import dotenv from "dotenv";
 import http from "http";
 import { Server } from "socket.io";
+import express from "express";
+import cors from "cors";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 import { startAllSchedulers } from "./jobs/index.js";
 import { setupWebSocket } from "./websockets/socketManager.js";
 
 dotenv.config();
-
 connectDB();
 
 const PORT = process.env.PORT || 5000;
+
+// UNIVERSAL EXPRESS CORS
+app.use(cors({
+  origin: "*",
+  methods: "*",
+  allowedHeaders: "*",
+  credentials: false
+}));
+
 const server = http.createServer(app);
+
+// UNIVERSAL SOCKET.IO CORS
 const io = new Server(server, {
   cors: {
-    origin: "*", // Adjust for production
-  },
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*"
+  }
 });
 
-setupWebSocket(server);
+// Pass socket instance to manager (NOT server!)
+setupWebSocket(io);
 
-// Socket.io connection handling
 io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+  console.log("Client connected:", socket.id);
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    console.log("Client disconnected:", socket.id);
   });
 });
 
-// Make io available globally or export it
 global.io = io;
 
 server.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
-  startAllSchedulers(); // Start all scheduled jobs
+  startAllSchedulers();
 });
