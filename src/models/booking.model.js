1ee9f4320ca8196de9ff51b155078cb5f9b829f2
samocology <godstimesml@gmail.com>
2026-03-20 21:31:39 +0100

    fixed reservation confirmation

--- src/models/booking.model.js
+++ src/models/booking.model.js
@@ -55,7 +55,7 @@ const bookingSchema = new mongoose.Schema({
     },
     paymentStatus: { 
         type: String,
-        enum: ['pending', 'paid', 'partly_paid', 'refunded', 'failed', 'not_paid', 'Paid'],
+        enum: ['pending', 'paid', 'partly_paid', 'refunded', 'failed', 'not_paid', 'Paid', 'Part Paid', 'Pay Later'],
         default: 'pending',
         required: true,
         index: true
